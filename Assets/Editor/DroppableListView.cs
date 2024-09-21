ed79c09ae5010e92a1c1fa50e265ce78901b2872
jianzhouyao <terrynoya@gmail.com>
2024-09-21 11:20:36 +0800

    first commit

---
+++ Assets/Editor/DroppableListView.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class DroppableListView
+    {
+        private IList _datas;
+        private Vector2 scrollPosition;
+        private bool _editable;
+        private const float DEFAULT_EMPTY_HEIGHT = 20f; // 默认空列表的高度
+
+        public Func<Object[],bool> OnCheckDrop;
+        public Action<Object> OnDropItem;
+        private float? _height;
+        
+        public readonly GUIStyle headerBackground = "RL Header";
+        private readonly GUIStyle emptyHeaderBackground = "RL Empty Header";
+        public readonly GUIStyle elementBackground = "RL Element";
+
+        public string Title { get; private set; }
+
+        public DroppableListView(IList datas,string title,bool editable = false,float? height = null)
+        {
+            _datas = datas;
+            _editable = editable;
+            _height = height;
+            Title = title;
+        }
+
+        public IList Datas => _datas;
+
+        private void DoListHeader(Rect headerRect)
+        {
+            DrawHeaderBackground(headerRect);
+            EditorGUI.LabelField(headerRect, Title);
+        }
+        
+        public void DoLayout()
+        {
+            // do the custom or default header GUI
+            GUIStyle boxStyle = new GUIStyle(EditorStyles.helpBox);
+            var headerHeight = 20;
+            Rect headerRect = GUILayoutUtility.GetRect(0, headerHeight, GUILayout.ExpandWidth(true));
+            DoListHeader(headerRect);
+            
+            if (_datas.Count == 0)
+            {
+                // 如果列表为空，显示提示信息
+                Rect emptyRect = EditorGUILayout.GetControlRect(false, DEFAULT_EMPTY_HEIGHT);
+                EditorGUI.HelpBox(emptyRect, "List is Empty", MessageType.Info);
+                DragDrop();
+            }
+            else
+            {
+                if (_height != null)
+                {
+                    scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition,boxStyle,GUILayout.Height(_height.Value));
+                }
+                else
+                {
+                    scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition,boxStyle);    
+                }
+                
+                for (int i = 0; i < _datas.Count; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    DrawCustomTextField(i);
+                    if (GUILayout.Button("Remove", GUILayout.Width(60)))
+                    {
+                        _datas.RemoveAt(i);
+                        GUIUtility.ExitGUI();
+                    }
+                    EditorGUILayout.EndHorizontal();
+                }
+                EditorGUILayout.EndScrollView();
+                DragDrop();
+            }
+        }
+
+        private void DragDrop()
+        {
+            Event evt = Event.current;
+            Rect dropArea = GUILayoutUtility.GetLastRect();
+            switch (evt.type)
+            {
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    OnDragPerform(dropArea,evt);
+                    break;
+            }
+        }
+
+        private void OnDragPerform(Rect dropArea,Event evt)
+        {
+            if (!dropArea.Contains(evt.mousePosition))
+                return;
+            var canDrop = OnCheckDrop?.Invoke(DragAndDrop.objectReferences) ?? true;
+            if (!canDrop)
+            {
+                return;
+            }
+            DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+            if (evt.type == EventType.DragPerform)
+            {
+                DragAndDrop.AcceptDrag();
+                foreach (var dropObject in DragAndDrop.objectReferences)
+                {
+                    // if (PrefabUtility.GetPrefabAssetType(draggedObject) != PrefabAssetType.NotAPrefab)
+                    // {
+                    //     
+                    // }
+                    OnDropItem?.Invoke(dropObject);
+                }
+            }
+        }
+
+        private void DrawCustomTextField(int index)
+        {
+            EditorGUILayout.LabelField(_datas[index].ToString());
+            var rect = EditorGUILayout.GetControlRect();
+            // if (Event.current.type == EventType.Repaint)
+            // {
+            //     elementBackground.Draw(rect,false,false,false,false);    
+            // }
+            // Event currentEvent = Event.current;
+            // if (currentEvent.type == EventType.DragUpdated || currentEvent.type == EventType.DragPerform)
+            // {
+            //     if (textFieldRect.Contains(currentEvent.mousePosition))
+            //     {
+            //         DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+            //         if (currentEvent.type == EventType.DragPerform)
+            //         {
+            //             DragAndDrop.AcceptDrag();
+            //             foreach (var draggedObject in DragAndDrop.objectReferences)
+            //             {
+            //                 // _datas[index] = AssetDatabase.GetAssetPath(draggedObject);
+            //             }
+            //         }
+            //         currentEvent.Use();
+            //     }
+            // }
+        }
+        
+        // draw the default header background
+        public void DrawHeaderBackground(Rect headerRect)
+        {
+            if (Event.current.type == EventType.Repaint)
+            {
+                // We assume that a height smaller than 5px means a header with no content
+                if (headerRect.height < 5f)
+                {
+                    emptyHeaderBackground.Draw(headerRect, false, false, false, false);
+                }
+                else
+                {
+                    headerBackground.Draw(headerRect, false, false, false, false);
+                }
+            }
+        }
+    }
---
+++ Assets/Editor/Test/TestWindow.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Object = System.Object;
+
+namespace Editor.Test
+{
+    public class TestWindow:EditorWindow
+    {
+        private DroppableListView _droppableListView;
+        private List<GameObject> _datas = new List<GameObject>();
+        
+        [MenuItem("yaojz/Droppable List")]
+        public static void Test()
+        {
+            var win = GetWindow<TestWindow>();
+            win.Init();
+            win.Show();
+        }
+        
+        private void Init()
+        {
+            _droppableListView = new DroppableListView(_datas,"Prefab列表");
+            _droppableListView.OnCheckDrop = OnDropItemCheck;
+            _droppableListView.OnDropItem = OnDropItem;
+        }
+        
+        private void OnDropItem(Object item)
+        {
+            _datas.Add(item as GameObject);
+        }
+
+        private bool OnDropItemCheck(Object[] items)
+        {
+            foreach (var item in items)
+            {
+                if (item.GetType() != typeof(GameObject))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        private void OnGUI()
+        {
+            _droppableListView.DoLayout();
+            if (GUILayout.Button("print data"))
+            {
+                PrintData();
+            }
+        }
+
+        private void PrintData()
+        {
+            var datas = _droppableListView.Datas;
+            foreach (var data in datas)
+            {
+                Debug.Log(data);
+            }
+        }
+    }
+}
